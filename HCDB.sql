bfa1cccde1a9b16f4d40edfdf9efc7cd049787b9
srssina <95973648+srssina@users.noreply.github.com>
2023-12-15 19:54:31 +0330

    Update HCDB.sql

--- HCDB.sql
+++ HCDB.sql
@@ -1,1 +1,18 @@
+CREATE DATABASE Hospital;
 
+Create table patient(
+  MRId is Not null,
+  Name varchar(128),
+  Gender varchar(16),
+  Age smallint(128),
+  RecordID
+  primary key (MRId)
+  foreign key (RecordID) REFERENCES record(RecordID)
+);
+
+create table record(
+  recordID is not null,
+
+
+  primary key (RecordID)
+  );
